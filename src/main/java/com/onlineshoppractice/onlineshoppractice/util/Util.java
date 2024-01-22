5b4f350c764bae37d98d1aca84c4b2ac03647194
ellisman31 <akos99@protonmail.com>
2024-01-22 22:40:03 +0100

    add missing annotation for Util

--- src/main/java/com/onlineshoppractice/onlineshoppractice/util/Util.java
+++ src/main/java/com/onlineshoppractice/onlineshoppractice/util/Util.java
@@ -1,9 +1,11 @@
 package com.onlineshoppractice.onlineshoppractice.util;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Date;
 
+@Component
 public class Util {
-
     public static Date getCurrentDate() {
         return new Date();
     }
